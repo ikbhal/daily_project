66dce25e01bbceb126bba20976a06a6b41538085
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-17 09:40:40 +0530

    add requirements

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
+class Project {
+	constructor(id, name, details, users){
+		this.id = id;
+		this.name = name;
+		this.details = name;
+		this.users = users;
+	}
+}
+let pcount = 0;
+let p1 = new Project(pcount, "untitle project", "write description here");
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>daily project</h1>
+	<p>used for free teacher </p>
+	<p>add , delete, edit, lists, search daily project, assign project</p>
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
